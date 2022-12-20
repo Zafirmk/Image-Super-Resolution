8b9149647cda5f61d33b274e0843a0dbc5461930
Zafirmk <zafirmk0@gmail.com>
2022-12-20 04:19:00 -0500

    Add comments

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from Libraries.Model import CNNModel
 from Libraries.Metrics import Metrics
 from Libraries.Prediction import Predictor
 from Libraries.Graphs import PlotGraphs
-
 import tensorflow as tf
 
 if __name__ == "__main__":
+
+    # Generate all low res images in raw_data/low_res
     ImageDowngrader("raw_data/high_res", "raw_data/low_res", 50)
+
+    # Load data with 90 - 10 split
     data_loader = DataLoader("raw_data", [0.9, 0.1])
+    
+    # Object carrying SSIM metric
     metrics = Metrics()
+
+    # Prediction class
     predictor = Predictor()
+
+    # Used to plot graphs seen in report file
     grapher = PlotGraphs()
 
+    # Fill up training/testing/validation sets
     low_res_train, low_res_test, low_res_val, high_res_train, high_res_test, high_res_val = data_loader.loadData()
     model = CNNModel((256,256,3))
 
