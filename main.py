296674daf6f575db82cecd3ab842ef2e478e8301
Zafirmk <zafirmk0@gmail.com>
2022-12-20 04:04:16 -0500

    Added softmax back lolz

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     low_res_train, low_res_test, low_res_val, high_res_train, high_res_test, high_res_val = data_loader.loadData()
     model = CNNModel((256,256,3))
 
-    choice = input("Do you want to train a new model? (Y/N) - If not, existing weights will be used")
+    choice = input("Do you want to train a new model? (Y/N) - If not, existing weights from model.h5 will be used\n")
     if choice.lower() == 'y':
         model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001), loss = 'mse', metrics = [metrics.SSIM, 'acc'])
         model.fit(low_res_train, high_res_train, epochs = 50, batch_size = 1, validation_data = (low_res_val,high_res_val))
@@ -26,6 +26,10 @@ if __name__ == "__main__":
 
     model.summary()
     
+    #Indivdual Prediction being made
+    # predictor.predictIndividual(model, low_res_test, high_res_test, metrics, 0)
+    predictor.predictTestSet(model, low_res_test, high_res_test, metrics, "temp")
+
     print("Shape of training images: ",high_res_train.shape)
     print("Shape of test images: ",high_res_test.shape)
     print("Shape of validation images: ",high_res_val.shape)
